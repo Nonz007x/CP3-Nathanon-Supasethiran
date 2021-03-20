a90039a08fcea7155dab6ca5fd4f3114b8ba71c0
Nonz007x <81010515+Nonz007x@users.noreply.github.com>
2021-03-20 15:54:24 +0700

    Add files via upload

---
+++ Exercise4_Nathanon_S.py
@@ -0,0 +1,10 @@
+FE = 100
+GB = 43
+ITCS = 18.5
+CP = 29
+
+print('--- Your Score ---')
+print('Foundation English               :' ,FE)
+print('General Business                 :' ,GB)
+print('Introduction to Computer Systems :' ,ITCS)
+print('Computer Programming             :' ,CP)
